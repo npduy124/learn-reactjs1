e141a82de3e6dbdbe7a6f883f990c015abe91e84
npduy <npduy124@gmail.com>
2025-12-14 12:26:13 +0700

    hello

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ha Noi
+          Ha Noi 2
         </p>
         
       </header>
